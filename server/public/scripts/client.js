c61919d706cef8d216ec5dbcabbd2647b9acf3b6
th30d0rab1e <darthtlc@gmail.com>
2017-07-17 10:49:17 -0500

    what i have so far

---
+++ server/public/scripts/client.js
@@ -0,0 +1,5 @@
+console.log('JS sourced');
+
+// First param link to <html ng-app="myApp">
+// [] is the dependency injection array, what services do we need?
+var myApp = angular.module('myApp', []); // Leave array empty for now
